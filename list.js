b2d04638fb1ded893fb196667ef56ac7a2f867b6
emi-co <200~158002903+emi-co@users.noreply.github.com>
2024-10-21 13:55:09 -0400

    Random checkbox functionality added

--- list.js
+++ list.js
@@ -1,27 +1,34 @@
+let counter = 0;
+
 //Create New List Item
 document.getElementById("Add").onclick = getNewItem;
 
 function getNewItem() {
+  counter++;
+
   // var li = document.createElement("li");
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.name = "name";
   checkbox.value = "value";
-  checkbox.id = "id";
+  checkbox.id = "checkbox-" + counter;
 
   var inputValue = document.getElementById("input").value;
   var label = document.createElement("label");
-  label.htmlFor = "id";
+  label.htmlFor = checkbox.id;
   label.appendChild(document.createTextNode(inputValue));
   var t = document.createTextNode(inputValue);
   // li.appendChild(t);
-  if (inputValue === "") {
-    alert("Error");
-  } else {
-    document.getElementById("list").appendChild(checkbox);
-  }
-  // document.getElementById("label").value = "";
-  document.getElementById("label").value = "";
+  // if (inputValue === "") {
+  //   alert("Error");
+  // } else {
+  document.getElementById("list").appendChild(checkbox);
+  document.getElementById("list").appendChild(label);
+
+  //clear
+  document.getElementById("input").value = "";
 }
+// document.getElementById("label").value = "";
+// }
 
 //toggle viewPurchased
