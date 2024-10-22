3bdd5b63e5b8098a513ad13672451b0e0a4e19a9
Yasin <yasinabdi.amir@gmail.com>
2024-10-22 10:05:59 -0400

    fixed js Purchased and Unpurchased

--- list.js
+++ list.js
@@ -3,11 +3,22 @@ function list() {
     const list = document.querySelector(".list");
     const grocery = document.querySelector(".grocery");
     const groceryValue = grocery.value;
-    const item = document.createElement("li");
+    const item = document.createElement("button");
     item.classList.add("item");
     item.textContent = groceryValue;
     list.appendChild(item);
     grocery.value = "";
+    item.addEventListener("click", () => switchToPurchased(item));
+    list.appendChild(item);
+    grocery.value = "";
+}
+
+function switchToPurchased(item) {
+    const purchasedSection = document.querySelector(".Purchased");
+    item.remove();
+    item.classList.add("purchased-item");
+    purchasedSection.appendChild(item);
+
 }
 
 
@@ -18,7 +29,6 @@ function add() {
 }
 
 add();
-
 
 
 
