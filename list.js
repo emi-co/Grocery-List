aa340a4018986b215a40876dfe4cbb09c27b3418
emi-co <200~158002903+emi-co@users.noreply.github.com>
2024-10-21 20:27:25 -0400

    Strikethrough complete

--- list.js
+++ list.js
@@ -11,24 +11,34 @@ function getNewItem() {
   checkbox.type = "checkbox";
   checkbox.name = "name";
   checkbox.value = "value";
-  checkbox.id = "checkbox-" + counter;
+  checkbox.id = "checkbox-" + counter; //Randomizes the ID
 
   var inputValue = document.getElementById("input").value;
   var label = document.createElement("label");
   label.htmlFor = checkbox.id;
-  label.appendChild(document.createTextNode(inputValue));
-  var t = document.createTextNode(inputValue);
-  // li.appendChild(t);
-  // if (inputValue === "") {
-  //   alert("Error");
-  // } else {
-  document.getElementById("list").appendChild(checkbox);
-  document.getElementById("list").appendChild(label);
+  var span = document.createElement("span");
+  span.textContent = inputValue;
+
+  // label.appendChild(document.createTextNode(inputValue));
+
+  var li = document.createElement("li"); //make checkboxes list items
+  li.appendChild(checkbox);
+  li.appendChild(span);
+
+  document.getElementById("list").append(li);
+
+  //strike-through
+
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      span.style.textDecoration = "line-through";
+    } else {
+      span.style.textDecoration = "none";
+    }
+  });
 
   //clear
-  document.getElementById("input").value = "";
+  document.getElementById(input).value = "";
 }
-// document.getElementById("label").value = "";
-// }
 
 //toggle viewPurchased
